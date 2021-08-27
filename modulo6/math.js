292c4c16f1fc21a87e303a25f8bd6537bace74a8
Wend Carlos <wendcarlos18@hotmail.com>
2021-08-27 10:30:01 -0300

    I created the module 6 folder and the math.js file

---
+++ modulo6/math.js
@@ -0,0 +1,8 @@
+/* Math */
+
+console.log(Math.PI) // MOSTRA O VALOR DE PI
+console.log(Math.pow(2,3)) // PERMITE CALCULAR POTENCIAÇÃO
+console.log(Math.sqrt(4,2)) // CACULA A RAIZ DE UM NÚMERO
+console.log(Math.random()) // MOSTRA UM NÚMERO ALEATÓRIO ENTRE 0 E 1
+console.log(Math.ceil(22.1)) // ARREDONDA UM NÚMERO PARA CIMA
+console.log(Math.floor(22.1)) // ARREDONDA UM NÚMERO PARA BAIXO
