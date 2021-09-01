5795ad4eb4cc9781949d1575e8f1db95e3baf795
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-01 17:09:20 -0300

    aula 25 e aula 26

---
+++ modulo6/for-in.js
@@ -0,0 +1,30 @@
+/*
+
+ESTRUTURA DE REPETIÇÃO - FOR IN
+
+*/
+/*
+const student = {
+    name: 'Carlos',
+    age: 25,
+    genre: 'male'
+}
+
+for(let propety in student){
+    console.log(`${propety}: ${student[propety]}`)
+}
+
+*/
+
+let peaple = {
+    name: 'José',
+    age: 50,
+    genre: 'male',
+    height: 1.7,
+    address: 'Rua 2 de Julho',
+    nationality: 'Brasileiro'
+}
+
+for(let propety in peaple) {
+    console.log(`${propety}: ${peaple[propety]}`)
+}
---
+++ modulo6/for-of.js
@@ -0,0 +1,21 @@
+/*
+
+ESTRUTURA DE REPETIÇÃO FOR OF
+
+*/
+
+let myName = 'Carlos'
+
+let allNames = ['João', 'Maria', 'José', 'Pedro']
+
+for(let names of allNames) {
+    console.log(names)
+}
+
+/* ========== Exemplo 2 ========== */
+
+let Numbers = [1, 2, 3, 4, 5]
+
+for(allNumber of Numbers) {
+    console.log(allNumber)
+}
