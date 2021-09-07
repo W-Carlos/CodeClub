0e838868da005ed24201db60d12e29f617ff45b8
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-07 17:16:56 -0300

    desafio05 e desafio06

---
+++ modulo6.1/desafios - JS03/desafio05.js
@@ -0,0 +1,16 @@
+/*[ ] Escreva um programa onde, você chame uma função mandando dois argumentos, 2 números, e a função responde qual número é maior*/
+
+function numbers(number1, number2) {
+    let result1 = Math.max(number1, number2)
+    let result2 = Math.min(number1, number2)
+
+    console.log(`O número maior é ${result1}`)
+    console.log(`O número menor é ${result2}`)
+
+    return 
+}
+
+let numberOne = 10
+let numberTwo = 4
+
+numbers(numberOne, numberTwo)
---
+++ modulo6.1/desafios - JS03/desafio06.js
@@ -0,0 +1,41 @@
+/*[ ] A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número de filhos. A prefeitura deseja saber: 
+    - média do salário da população; 
+    - média do número de filhos; 
+    - maior salário; 
+    - O final da leitura de dados se dará com a entrada de um salário negativo. 
+    - Faça isso usando uma função!*/
+
+let population = [
+    person1 = {
+        wage: 1500,
+        sons: 3
+    }, 
+
+    person2 = {
+        wage: 2000,
+        sons: 4
+    },
+
+    person3 = {
+        wage: 1200,
+        sons: 2
+    },
+
+    person4 = {
+        wage: 3000,
+        sons: 1
+    }, 
+
+    person5 = {
+        wage: -400,
+        sons: 3
+    }
+]
+
+function media(number1) {
+    
+    //console.log(population)
+    return
+}
+
+media(population)
