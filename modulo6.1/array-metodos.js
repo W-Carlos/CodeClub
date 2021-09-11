49fd7105c32827f218ea4f2ad2fe1f115ab0fbf5
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-11 17:09:37 -0300

    Create array-metodos.js

---
+++ modulo6.1/array-metodos.js
@@ -0,0 +1,17 @@
+/* Arrays - Metodos */
+
+const students = ['João', 'Maria', 'Pedro', 'Carla', 'Bia', 'Mauricio']
+
+// push = Vai adicionar um item no final do array.
+students.push('Carlos')
+
+// length = Mostra o tamanho do array
+console.log(students.length)
+
+// sort = Coloca os elementos em ordem alfabetica
+console.log(students.sort())
+
+// delete = Deleta um item do array
+delete students[0]
+
+console.log(students)
