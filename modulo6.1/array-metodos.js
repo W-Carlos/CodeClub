3e834927190e1a769a5b7eaac8fd2ee72b5b5875
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-12 16:49:20 -0300

    array-metodos.js

--- modulo6.1/array-metodos.js
+++ modulo6.1/array-metodos.js
@@ -14,4 +14,17 @@ console.log(students.sort())
 // delete = Deleta um item do array
 delete students[0]
 
+// splice (unir / ligar) = Permite que tire ou coloque itens no array exatamente na posição que queremos
+students.splice(0, 1) // Retira um ou mais itens
+students.splice(1, 0, 'Carlos') // Adiciona item na posição 1
+
+// slice (fatiar) = Permite que tire itens do array
+const newStudents = students.slice(1, 3)
+
+// pop = Esse metodo retira o ultimo item do array
+students.pop()
+
+// shift = Retira o primeiro item do array
+students.shift()
+
 console.log(students)
