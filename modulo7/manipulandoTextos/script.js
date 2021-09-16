4289673b6fa2d991cdc44f7bdc6c99a314270f05
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-16 20:16:08 -0300

    create manipulandoTextos and selecionandoAtributos

---
+++ modulo7/manipulandoTextos/script.js
@@ -0,0 +1,6 @@
+const element = document.querySelector('h1')
+
+//element.innerText = "Hello World" // Só manipula o texto
+//element.textContent = "Olá, Mundo" // Só manipula o texto
+
+element.innerHTML = "Hello World <p>Adicionando HTML</p> " // Manipula o html 
---
+++ modulo7/selecionandoAtributos/script.js
@@ -0,0 +1,3 @@
+const element = document.querySelector('input')
+// Pegando os atributos
+console.log(element.value)
