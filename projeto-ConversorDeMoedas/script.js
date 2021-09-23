67f3583c3640cded9b68da0b98e452d4a06675f4
Wend Carlos <wendcarlos18@hotmail.com>
2021-09-23 18:47:45 -0300

    Create file modulo9

--- projeto-ConversorDeMoedas/script.js
+++ projeto-ConversorDeMoedas/script.js
@@ -1,9 +1,9 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.2
-const euro = 5.9
-const bitcoin = 0.0000039
+const dolar = 5.29
+const euro = 6.20
+const bitcoin = 255094.88
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
@@ -26,6 +26,12 @@ const convertValues = () => {
             { style: 'currency', currency: 'EUR' }
         ).format(inputReais / euro)
     }
+
+    if(select.value === "Bitcoin"){
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'XBT' }
+        ).format(inputReais / bitcoin)
+    }
     
 }
 
@@ -41,6 +47,11 @@ const changeCurrency = () => {
     if(select.value === "€ Euro") {
         currencyName.innerHTML = 'Euro'
         currencyImg.src = 'assets/euro.svg'
+    }
+
+    if(select.value === "Bitcoin") {
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = 'assets/bitcoin.svg'
     }
 
     convertValues()
