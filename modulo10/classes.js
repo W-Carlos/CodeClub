bcd86d6c2ad1c19a18746adfc10ada4a3a18c27c
Wend Carlos <wendcarlos18@hotmail.com>
2021-10-07 17:15:45 -0300

    classes

--- modulo10/classes.js
+++ modulo10/classes.js
@@ -11,12 +11,44 @@ const person = {
     }
 }*/
 
+// Classes sempre devem ser escritas com letras maiusculas.
+
+/*
 class Person {
     name;
     age;
 
     talk(){
-        console.log(`Hello, my name is ${this.name} and Im ${this.age}`)
+        console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old`)
+    }
+}
+
+const newPerson = new Person()
+
+newPerson.name = "Carlos"
+newPerson.age = 25
+newPerson.talk()*/
+
+// Podemos usar com o metodo constructor
+class Person {
+    constructor(name, age){
+        console.log(`Hello, I'm ${name}, I'm ${age} year old`)
+
+        this.name = name
+        this.age = age
     }
 
+    talk(){
+        console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old`)
+    }
 }
+
+const newPerson = new Person("Carlos", 25)
+const newPerson1 = new Person("Maria", 30)
+const newPerson2 = new Person("Pedro", 18)
+const newPerson3 = new Person("José", 40)
+
+newPerson.talk()
+newPerson1.talk()
+newPerson2.talk()
+newPerson3.talk()
